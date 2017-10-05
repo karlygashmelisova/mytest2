e207e64a07aa7927ac418fbeadeb7a98fc89869d
karlygashmelisova <32523651+karlygashmelisova@users.noreply.github.com>
2017-10-06 02:16:16 +0600

    Ozgeris githubta

--- src/kz/kaznitu/Hello.java
+++ src/kz/kaznitu/Hello.java
@@ -6,6 +6,11 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("Bul zertkhanalyk jumys maksaty:");
         System.out.println("GitHub - pen jumys jasap uirenu.");
+           int a = MyMath.kvadrat(5);
+        int b = MyMath.kub(5);
+        
+        System.out.println(" a = " + a);
+        System.out.println(" b = " +b);
     }
 }
 
