9d27307af67ca249d8376d4138404376a89524ed
Melisova Karlygash <melisovakarlygash@gmail.com>
2017-10-06 01:23:11 +0600

    tortinwi ozgeris

--- src/kz/kaznitu/MyMath.java
+++ src/kz/kaznitu/MyMath.java
@@ -7,5 +7,19 @@ public class MyMath {
     public static int kub(int a){
         return a*a*a;
     }
+    public static boolean isEven(int a){
+        return (a % 2 == 0);
 
 }
+    public static int max(int a, int b) {
+        if (a >= b)
+            return a;
+        else
+            return b;
+    }
+    public static int min(int a, int b) {
+        if (a >= b)
+            return b;
+        else
+            return a;
+    }}
