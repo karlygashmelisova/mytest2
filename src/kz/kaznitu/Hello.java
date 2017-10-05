c685930dde0f524dad96b19d3e7336a720bc3513
Melisova Karlygash <melisovakarlygash@gmail.com>
2017-10-06 00:52:57 +0600

    men ozgeris engizdim

--- src/kz/kaznitu/Hello.java
+++ src/kz/kaznitu/Hello.java
@@ -2,7 +2,10 @@ package kz.kaznitu;
 
 public class Hello {
     public static void main(String[] args){
-    System.out.println("Hello World");
+
+        System.out.println("Hello World");
+        System.out.println("Bul zertkhanalyk jumys maksaty:");
+        System.out.println("GitHub - pen jumys jasap uirenu.");
     }
 }
 
